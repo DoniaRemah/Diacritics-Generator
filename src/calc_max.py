6d087b9919535602f2bdfddc9eeee9ab74455ba7
Ola Abouelhadid <abouelhadid.ola@gmail.com>
2024-01-01 02:00:41 +0200

    calc max and model prediction

--- src/calc_max.py
+++ src/calc_max.py
@@ -1,13 +1,30 @@
 import utils
 import globals
+import models_prediction
 
 # load the data 
-globals.word_embeddings=utils.loadPickle('output_from0to10000/word_embeddings_0_5000.pickle')
-globals.char_embeddings=utils.loadPickle('output_from0to10000/char_embeddings_0_5000.pickle')
+globals.word_embeddings=utils.loadPickle('output/word_embeddings_20000_25000.pickle')
+globals.char_embeddings=utils.loadPickle('output/char_embeddings_20000_25000.pickle')
 
+models_prediction.extract_chars()
 max_sentence_length = max(len(seq) for seq in globals.word_embeddings) -2
 
-max_word_length = max(max(len(word) for word in sentence) for sentence in globals.char_embeddings)
+max_word_length = max(max(len(word) for word in sentence) for sentence in globals.model_char_embeddings)
 
 print("max sentence length is: ", max_sentence_length)
 print("max word length is: ", max_word_length)
+
+# max sentence length is:  740
+# max word length is:  10
+
+# max sentence length is:  1708
+# max word length is:  10
+
+# max sentence length is:  363
+# max word length is:  10
+
+# max sentence length is:  314
+# max word length is:  11
+
+# max sentence length is:  323
+# max word length is:  10
